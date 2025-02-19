a06ed892cbd3e94719c3832e2f88c90c22526689
初木清 <1398015278@qq.com>
2025-02-19 08:05:02 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,45 @@
 /*
+// Basic warp usage
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+def getPoints(event,x,y,flags,param):
+    global click_times,row,col,points
+    if event==cv2.EVENT_LBUTTONDOWN:
+        click_times+=1
+        points[click_times-1]=[x,y]#将点击点的左边传给我们的points，用来变换的第一批参数
+        cv2.circle(img,(x,y),4,(25,25,255),-1)#标记我们点击的位置信息
+        cv2.rectangle(img,(col-60,row-20),(col,row),(255,255,255),-1)#空出一小块地方用来现实鼠标点击的位置
+        cv2.putText(img,'%d,%d'%(x,y),(col-60,row-8),cv2.FONT_HERSHEY_SIMPLEX,0.4,(0,0,0),1)
+		
+
+path = r'D:\PythonScripts\test.png'
+img = cv2.imdecode(np.fromfile(path, dtype=np.uint8), -1)
+
+resized_img = cv2.resize(img, None, fx=0.5, fy=0.5)
+
+cv2.namedWindow('image')
+cv2.setMouseCallback('image', getPoints)
+
+click_times = 0
+points=np.float32([[0,0],[0,0],[0,0],[0,0]])
+row, col, _ = img.shape
+
+while(1):
+    cv2.imshow('image',img)
+    if cv2.waitKey(10)& 0xFF==ord('q') or click_times>=4:#当按下q或者满4个点退出，进行变换
+        break
+cv2.destroyAllWindows()
+
+pts2=np.float32([[0,0],[300,0],[0,300],[300,300]])#输出图像的分辨率
+M=cv2.getPerspectiveTransform(points,pts2)
+dst=cv2.warpPerspective(img,M,(300,300))#透视变换
+print(points)
+cv2.imwrite(r'D:\PythonScripts\test1.png', dst)
+*/
+
+/*
 // 在不阻塞线程的前提下读取日志详细
 import subprocess
 import threading
